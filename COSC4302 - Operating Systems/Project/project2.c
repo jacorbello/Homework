9ba7f1a40003484df1b4807975a1269c9cb74329
Jeremy Corbello <Jeremy@jeremycorbello.com>
2018-12-02 22:10:48 -0600

    Updated parseCommand function

--- COSC4302 - Operating Systems/Project/project2.c
+++ COSC4302 - Operating Systems/Project/project2.c
@@ -6,9 +6,10 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 char *getPath(char **, char **);
-int parseCommand(char *, struct command_t *);
+int parseCommand(const char *, const char, char **);
 int parsePath(char **);
 void printPrompt();
 void getCommand(char *);
@@ -70,22 +71,49 @@ void getCommand(char *commandLine)
     fgets(commandLine, LINE_LEN, stdin);
 }
 
-int parseCommand(char *commandLine, struct command_t *command)
-{
-    int argc = 0;
-    char **commandLinePtr;
 
-    commandLinePtr = &commandLine;
-    command->argv[argc] = (char *)malloc(MAX_ARG_LEN);
 
-    /* TODO: fix the integer to pointer declaration in the while(...=...) */
-    while (command->argv[argc] = strsep(commandLinePtr, WHITESPACE) != NULL)
-    {
-        command->argv[++argc] = (char *)malloc(MAX_ARG_LEN);
+int parseCommand(const char *s, const char delimiters, char **argvp){
+
+    int error;
+    int i;
+    int numtokens;
+    const char *snew;
+    char *t;
+
+    if ((s == NULL) || (delimiters == NULL) || (argvp == NULL)){
+        errno = EINVAL;
+        return -1;
     }
-    command->name = (char *)malloc(sizeof(command->argv[0]));
-    strcpy(command->name, command->argv[0]);
-    return 1;
+    argvp = NULL;
+
+    snew = s + strspn(s,delimiters);
+    if ((t=malloc(strlen(snew)+1))==NULL){
+        return -1;
+    }
+    strcpy(t, snew);
+    numtokens =0;
+    if(strtok(t, delimiters) != NULL){
+        for(numtokens =1; strtok(NULL, delimiters) != NULL; numtokens++);
+    }
+    if((argvp = malloc((numtokens + 1)*sizeof(char *))) == NULL){
+        error = errno;
+        free(t);
+        errno = error;
+        return -1;
+    }
+    if(numtokens == 0){
+        free(t);
+    }
+    else{
+        strcpy(t, snew);
+        **argvp = strtok(t, delimiters);
+        for(i=1; i<numtokens; i++){
+            *((argvp)+i) = strtok(NULL, delimiters);
+        }
+    }
+    *((argvp)+numtokens) = NULL;
+    return numtokens;
 }
 
 int parsePath(char *pathv[])
